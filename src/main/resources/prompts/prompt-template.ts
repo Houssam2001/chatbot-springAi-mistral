a5cb40417cd7098e12d47ed42ef6235b2bacc308
Houssam2001 <houssam2001mchmch@gmail.com>
2024-07-20 20:29:24 +0100

    first commit

---
+++ src/main/resources/prompts/prompt-template.ts
@@ -0,0 +1,6 @@
+ Answer the provided question using only the given context.
+ If you do not have the answer, answer only with : HATI OURSENGH MAY TENNIT
+ CONTEXT:
+ {context}
+ QUESTION:
+ {question}
